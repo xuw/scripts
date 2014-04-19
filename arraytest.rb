fa2ac41913951d281a0af72dc15a743e19f359b8
Wei Xu <wei.xu.0@gmail.com>
2014-04-20 00:41:36 +0800

    all-to-all ping

---
+++ arraytest.rb
@@ -0,0 +1,7 @@
+arr = Array.new(3){ Array.new(3) {Array.new}}
+
+arr[0][1] << 6
+arr[0][1] << 88
+p arr
+
+print "%2.2f" % (3.14159265)
--- ping_test.rb
+++ ping_test.rb
@@ -1,29 +1,69 @@
 require 'net/ssh'
-report = {}
 threads = []
-1.upto 180 do |num| 
+
+start_ip = 1
+end_ip = 180
+
+total = end_ip - start_ip + 1
+
+results = Array.new(total){ Array.new(total) {Array.new}}
+
+start_ip.upto end_ip do |num|
   t = Thread.new do
     begin
-     Net::SSH.start("10.1.0.#{num}", "root", :password => "root123", :auth_methods => ['password']) do |ssh|   
-       1.upto 180 do |dest| 
-         out = ssh.exec!("ping -c 8 10.1.0.#{dest}")
-         puts out
-       end
+     ip = "10.1.0.#{num}"
+     Net::SSH.start(ip, "root", :password => "root123", :auth_methods => ['password'], :timeout => 10) do |ssh|
+         puts "running on #{ip}"
+         out = ssh.exec!("seq #{start_ip} #{end_ip} | xargs -i{} -P 90 ping -A -w 10 -c 8 10.1.0.{}")
+
+         out.each_line do |line|
+           if (line =~ /.* bytes from (.*): icmp_seq=(.*) ttl=.* time=(.*) ms/)
+
+
+             seq = $2.to_i
+             time = $3.to_f
+
+             dest_ip = $1
+             dest_ip =~ /10.1.0.(.*)/
+             dest = $1.to_i - start_ip
+
+             from_ind = num.to_i - start_ip
+
+             # puts "from #{ip} to #{dest}: #{time} ms"
+
+             results[from_ind][dest] << time
+           end
+         end
 	#puts out
     end
   rescue
     puts "failed on #{num} #{$!}"
   end
 end
-  
   threads << t
-  sleep(0.2)
-end
-threads.each do |t|
-  puts "wating for slow process.... " until t.join(30)
+  sleep(0.1)
 end
 
-puts "Failed On Nodes:"
-report.sort.reject {|k, v| v[1] == 0}.each do |k, v|
-  p v
+
+sleep 200
+
+threads.each do |t|
+  t.exit
+end
+
+# threads.each do |t|
+#   puts "wating for slow process.... " until t.join(3)
+# end
+
+# p results
+
+include Enumerable
+cnt = start_ip
+results.each do |row|
+  row.each do |col|
+    avg_time = col.reduce(:+).to_f / col.size
+    print "%2.2f " % avg_time
+  end
+  print "\##{cnt}\n"
+  cnt += 1
 end
